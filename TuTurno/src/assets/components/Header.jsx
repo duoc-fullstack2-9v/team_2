2804c200aece86ecafd52101b39f5a7fd47939e5
steph4nia97 <step.munoz@duocuc.cl>
2025-10-08 17:39:18 -0300

    add components,imagenes

---
+++ TuTurno/src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Tu Turno</h1>
+      <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Calendario</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
