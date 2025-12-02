d043edd3eac46b54603ef463b06ca025349731ab
steph4nia97 <step.munoz@duocuc.cl>
2025-12-02 12:37:06 -0300

    api/session.js-configback-adaptacion calendario/login.jsx

--- backend/src/main/java/com/tuturno/backend/controller/AuthController.java
+++ backend/src/main/java/com/tuturno/backend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.tuturno.backend.controller;
 
+
 import com.tuturno.backend.dto.AuthResponse;
 import com.tuturno.backend.dto.LoginRequest;
 import com.tuturno.backend.dto.RegisterRequest;
@@ -7,21 +8,26 @@ import com.tuturno.backend.entity.Usuario;
 import com.tuturno.backend.service.UsuarioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*") // para permitir llamadas desde tu frontend React
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
+
     private final UsuarioService usuarioService;
+
 
     // Registro de usuario nuevo
     @PostMapping("/registro")
     public ResponseEntity<?> registro(@Valid @RequestBody RegisterRequest request) {
+
 
         Usuario usuario = Usuario.builder()
                 .nombre(request.getNombre())
@@ -34,25 +40,32 @@ public class AuthController {
                 .rol("ROLE_USER")
                 .build();
 
+
         Usuario creado = usuarioService.registrarUsuario(usuario);
+
 
         return ResponseEntity.ok("Usuario registrado con ID: " + creado.getId());
     }
+
 
     //login usuario
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
 
+
         Usuario usuario = usuarioService.buscarPorCorreo(request.getCorreo())
                 .orElseThrow(() -> new RuntimeException("Credenciales inválidas."));
+
 
         if (!usuarioService.verificarPassword(request.getPassword(), usuario.getPassword())) {
             throw new RuntimeException("Credenciales inválidas.");
         }
 
+
         // Aquí más adelante generaremos el JWT real.
         // Por ahora devolvemos un token "falso" de ejemplo.
         String tokenFalso = "TOKEN_DEMO_A_REEMPLAZAR_POR_JWT";
+
 
         AuthResponse response = new AuthResponse(
                 tokenFalso,
@@ -60,6 +73,7 @@ public class AuthController {
                 usuario.getNombre(),
                 usuario.getRol()
         );
+
 
         return ResponseEntity.ok(response);
     }
